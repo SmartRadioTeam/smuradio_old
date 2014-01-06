55c2a5f087ab6c81de9743fd2c2907b4f9c0f795
qwe7002 <381511791@qq.com>
2014-01-06 18:43:38 +0800

    4.0.5

--- class/conf.php
+++ class/conf.php
@@ -2,6 +2,8 @@
 /*请将以下信息替换成您的信息*/
 define("PROJECTNAME", "网站名称"); //网站名称
 define("PASSWORD", "后台密码");//后台密码
+define("IMGURL", "/");//pc端图片服务器地址（可以为绝对路径或者相对路径）
+define("IMGRAND", "pc端底图数量");//pc端底图数量
 define("MYSQLUSER", "数据库用户名");//数据库用户名
 define("MYSQLPASSWORD", "数据库密码");//数据库密码
 define("MYSQLDB", "数据库名称");//数据库名称
--- class/img.php
+++ class/img.php
@@ -1,7 +1,9 @@
 <div id="lay_bg" class="lay_background"">
 <?php
+include("../../class/conf.php");
 srand();
-$rand=rand(1,11);
-echo '<img class="lay_background_img" id="lay_bg_img" src="http://img.smxybbs.net/radio/'.$rand.'.jpg"></div>
+$rand=rand(1,IMGRAND);
+$rand=IMGURL.'/'.$rand.".jpg";
+echo '<img class="lay_background_img" id="lay_bg_img" src="'.$rand.'"></div>
 ';
 ?>
